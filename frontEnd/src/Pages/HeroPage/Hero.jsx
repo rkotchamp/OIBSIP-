2558ca8f77195d3785df3cf1271781ceebc5d690
Reuben <treuben90@yahoo.com>
2024-02-06 21:39:38 +0200

    created the carousel for the home page

--- frontEnd/src/Pages/HeroPage/Hero.jsx
+++ frontEnd/src/Pages/HeroPage/Hero.jsx
@@ -1,9 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./Hero.css";
 
 function Hero({ data }) {
-  const [slides, setSlides] = useState(0);
-  console.log(data);
+  const [slides, setSlides] = useState(2);
+
+  const nextSlides = () => {
+    setSlides(slides === data.length - 1 ? 0 : slides + 1);
+  };
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      nextSlides();
+    }, 8000);
+    return () => clearInterval(intervalID);
+  });
+
   return (
     <div className="hero__container">
       <div className="color first__gradient"></div>
@@ -13,9 +23,37 @@ function Hero({ data }) {
           return (
             <>
               <div className="textHeader">
-                <h1>{images.textHeader}</h1>
+                <h1
+                  className={
+                    slides === i
+                      ? "headersLetters"
+                      : "headersLetters slide_hidden"
+                  }
+                >
+                  {images.textHeader}
+                </h1>
               </div>
-              <img src={images.src} alt="" key={i} className="slides" />;
+              <img
+                src={images.src}
+                alt=""
+                key={i}
+                className={slides === i ? "image slides" : "image slide_hidden"}
+              />
+              <span className="indicators">
+                {data.map((_, i) => {
+                  return (
+                    <button
+                      onClick={() => setSlides(i)}
+                      className={
+                        slides === i
+                          ? "indicator-btn active-indicator"
+                          : "indicator-btn"
+                      }
+                      key={i}
+                    ></button>
+                  );
+                })}
+              </span>
             </>
           );
         })}
