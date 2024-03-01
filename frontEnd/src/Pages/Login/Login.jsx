93338d58a61cae77e8fa811e68ae61f5b8b90d08
Reuben <treuben90@yahoo.com>
2024-03-01 13:18:38 +0200

    created the login

--- frontEnd/src/Pages/Login/Login.jsx
+++ frontEnd/src/Pages/Login/Login.jsx
@@ -1,24 +1,47 @@
-import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import api from "../../api/api";
+import Cookies from "js-cookie";
+import UserContext from "./../../Context/UserContext";
 import "./Login.css";
 
-const handleLoginSubmit = (e) => {
-  e.preventDefault();
-
-  const form = new FormData(e.currentTarget);
-  const body = {};
-  if (!form.get("email") || !form.get("password")) {
-    console.log("Fill; out spaces");
-  } else {
-    for (const [key, value] of form.entries()) {
-      body[key] = value;
-    }
-
-    // i will post it to the api
-    console.log(body);
-  }
-};
-
 function Login() {
+  const [formError, setFormError] = useState("");
+  const [errorCheck, setErrorCheck] = useState(false);
+  const navigate = useNavigate();
+
+  const { setUser } = useContext(UserContext);
+
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+    const form = new FormData(e.currentTarget);
+    const body = {};
+    if (!form.get("email") || !form.get("password")) {
+      setErrorCheck(true);
+      setFormError("Fill out spaces");
+    } else {
+      setErrorCheck(false);
+      for (const [key, value] of form.entries()) {
+        body[key] = value;
+      }
+
+      api.post("/login", body).then((response) => {
+        if (response.status === 200) {
+          Cookies.set("user_token", response.data.token);
+          let config = {
+            headers: {
+              Authorization: "Bearer " + response.data.token,
+            },
+          };
+
+          api.get("/user", config).then((res) => {
+            setUser(res.data.data);
+            navigate("/");
+          });
+        }
+      });
+    }
+  };
   return (
     <div className="signUp__container">
       <div className="grading one__gradient"></div>
@@ -50,6 +73,7 @@ function Login() {
             </Link>
           </div>
           <button className="signBTN">Login</button>
+          {errorCheck && <p>{formError}</p>}
         </form>
       </div>
     </div>
